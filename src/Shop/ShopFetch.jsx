44430d193080bb8164e53f2b2a92a20e205f12a1
Carlos <110745188+carlDguez19@users.noreply.github.com>
2025-11-26 13:43:52 -0700

    pushed fetch calls into the array so i can use promise.all

--- src/Shop/ShopFetch.jsx
+++ src/Shop/ShopFetch.jsx
@@ -8,9 +8,7 @@ export function Shop(){
         async function fetchProducts(){
             const prodArr = [];
             for(let i = 1; i <= 20; i++){
-                fetch(`https://fakestoreapi.com/products/${i}`)
-                .then(response => response.json())
-                .then(data => prodArr.push(data));
+                prodArr.push(fetch(`https://fakestoreapi.com/products/${i}`).then(response => response.json()));
             }
             const results = await Promise.all(prodArr);
             setProducts(results);
