c26ba34ebf4f232075e0079f3b12dce8e969bc01
Carlos <110745188+carlDguez19@users.noreply.github.com>
2025-11-26 14:05:54 -0700

    im gonna fetch the wholeproduct catalog and then get a subset of the returned array

--- src/Shop/ShopFetch.jsx
+++ src/Shop/ShopFetch.jsx
@@ -6,13 +6,33 @@ export function Shop(){
 
     useEffect(() => {
         async function fetchProducts(){
+            try{
             const prodArr = [];
-            for(let i = 1; i <= 20; i++){
-                prodArr.push(fetch(`https://fakestoreapi.com/products/${i}`).then(response => response.json()));
+            const res = await fetch('https://fakestoreapi.com/products');
+            const data = await res.json();
+            console.log(data);
+            }catch(err){
+                console.error("error getting products: ", err);
             }
-            const results = await Promise.all(prodArr);
-            setProducts(results);
         }
         fetchProducts();
-    },[])
+    },[]);
+    
+    
+    // useEffect(() => {
+    //     async function fetchProducts(){
+    //         try{
+    //             const prodArr = [];
+    //             for(let i = 1; i <= 20; i++){
+    //                 prodArr.push(fetch(`https://fakestoreapi.com/products/${i}`).then(response => response.json()));
+    //             }
+            
+    //             const results = await Promise.all(prodArr);
+    //             setProducts(results);
+    //         }catch(err){
+    //             console.log("error fetching products: ", err);
+    //         }
+    //     }
+    //     fetchProducts();
+    // },[])
 }
