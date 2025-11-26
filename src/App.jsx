a208526ba4a4c59e395ae90feb588985981d24b1
Carlos <110745188+carlDguez19@users.noreply.github.com>
2025-11-26 12:58:25 -0700

    working on fetching from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Link, Outlet } from "react-router-dom"
 import { Nav } from "./NavBar"
 export function App() {
-  //ill put nav here and i can perhaps fetch from the api here aswell
   return (
     <>
       <Nav/>
--- src/Shop/ShopFetch.jsx
+++ src/Shop/ShopFetch.jsx
@@ -1,3 +1,17 @@
+import { useEffect, useState } from "react"
 export function Shop(){
     return <h1>Were gonna go..... SHOPPING!!!</h1>
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function fetchProducts(){
+            const prodArr = [];
+            for(let i = 1; i <= 20; i++){
+                fetch('https://fakestoreapi.com/products/${i}')
+                .then(response => response.json())
+                .then(data => prodArr.push(data));
+            }
+        }
+    })
 }
