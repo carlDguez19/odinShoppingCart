747d766660ef1443ae871be09690191b35a79320
Carlos <110745188+carlDguez19@users.noreply.github.com>
2026-01-07 18:13:53 -0700

    added a bunch of packages to be able to do some testing

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -4,20 +4,19 @@ import styles from "./Cart.module.css"
 
 export function Cart(){
     const {setTotItems, products, setProducts} = useOutletContext();
-    console.log(products);
+    //console.log(products);
+
     function removeFromCart(id){
         let remItemQuantity = 0;
-        setProducts(prev => {
-            const newProdArr = prev.map(prod => {
-                if(prod.id === id){
-                    remItemQuantity = prod.quantity;
-                    return {...prod, inCart: false, quantity: 0};
-                }
-                return prod;
-            });
-            return newProdArr;
+        const newProdArr = products.map(prod =>{
+            if(prod.id === id){
+                remItemQuantity = prod.quantity;
+                return {...prod, inCart: false, quantity: 0};
+            }
+            return prod;
         });
-        setTotItems(prev => prev - remItemQuantity);
+        setProducts(newProdArr);
+        setTotItems(prev => prev-remItemQuantity);
     }
 
     return (
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,3 +1,3 @@
 export function Home(){
-    return <h1>Home page bruh</h1>
+    return <h1>Welcome to the coolest of online shops with the simplest of home pages!</h1>
 }
